27cca627ac4cebeec50142ea1a451ddbfb3b17e3
weijiewen <weijiewen@weijiewendeMacBook-Pro.local>
2023-05-25 23:18:05 +0800

    3.8

--- FLKit/src/main/java/com/wjw/flkit/base/FLBaseActivity.java
+++ FLKit/src/main/java/com/wjw/flkit/base/FLBaseActivity.java
@@ -924,7 +924,7 @@ public abstract class FLBaseActivity extends FragmentActivity implements View.On
         layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
         view.setLayoutParams(layoutParams);
         view.setVisibility(View.VISIBLE);
-        annexLayout.addView(view);
+        superLayout.addView(view, superLayout.indexOfChild(annexLayout));
     }
     public final void removeFullView(View view) {
         superLayout.removeView(view);
