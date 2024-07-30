1a086b48b674ef52a316949d67e53a0d01501d11
weijiewen <txywjw@icloud.com>
2024-07-30 18:01:58 +0800

    5.7

--- FLKit/src/main/java/com/wjw/flkit/base/FLBindingActivity.java
+++ FLKit/src/main/java/com/wjw/flkit/base/FLBindingActivity.java
@@ -15,7 +15,9 @@ public abstract class FLBindingActivity<T extends ViewBinding> extends FLBaseAct
         binding = getBinding();
         return binding.getRoot();
     }
-    protected abstract void configNavigation(FLNavigationView navigationView);
+    protected void configNavigation(FLNavigationView navigationView) {
+
+    }
     protected abstract T getBinding();
     protected abstract void didLoad();
     protected abstract void didClick(View view);
